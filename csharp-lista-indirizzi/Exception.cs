5765d1e32eb731cfc069359a31eb2cb7ff529767
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-14 17:11:22 +0200

    Scheletro dell'esercizio (ho sonno)

---
+++ csharp-lista-indirizzi/Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Exception
+{
+	public Exception() {}
+	
+
+	public class WrongListingException : Exception
+	{
+		public WrongListingException(string message) : base(message) {}
+	}
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace csharp_lista_indirizzi
+﻿using System.Linq.Expressions;
+
+namespace csharp_lista_indirizzi
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Address> addressList = new List<Address>(Path );
+
+            try 
+            {
+                Address();
+                foreach (var address in addressList)
+                {
+                    Console.WriteLine($"{address.name}, {address.surname}");
+                }
+            } 
+            catch (WrongListingException ex) 
+            {
+                Console.WriteLine($"Errore nella compilazione della lista {ex.Message}");
+            }
+            
         }
-    }
 }
